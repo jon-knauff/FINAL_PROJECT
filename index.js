af7e1fb81eeb69403faf3fcada7381e7a49a011b
jon-knauff <jon.knauff@comcast.net>
2024-09-09 14:48:11 -0600

    fdf

--- index.js
+++ index.js
@@ -30,3 +30,23 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+document.getElementById('burger-menu').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.add('open');
+    document.getElementById('overlay').style.display = 'block';
+    setTimeout(() => document.getElementById('overlay').style.opacity = 1, 10);
+});
+
+document.getElementById('closebtn').addEventListener('click', function() {
+    closeSidebarAndOverlay();
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    closeSidebarAndOverlay();
+});
+
+function closeSidebarAndOverlay() {
+    document.getElementById('sidebar').classList.remove('open');
+    document.getElementById('overlay').style.opacity = 0;
+    setTimeout(() => document.getElementById('overlay').style.display = 'none', 300); // Match transition time
+}
+
